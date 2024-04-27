61c4da4cf72649c14d73c38914cebc7907bc6586
Tiago <ti.almeida58@gmail.com>
2024-04-27 20:33:22 -0300

    chore: install and configuration styled-components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+'use client';
+
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Barbearia",
-  description: "Barber shop app",
-};
+import {Providers} from "@/app/Providers/providers";
 
 export default function RootLayout({
   children,
@@ -16,7 +10,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
